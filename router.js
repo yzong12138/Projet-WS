7f4686567cd962f8cac69728219ec6ec8150cd4b
yzong12138 <yuxuan.zong@insa-lyon.fr>
2020-12-02 08:09:01 +0100

    feat:add the function of the datalist

--- router.js
+++ router.js
@@ -3,7 +3,8 @@ var router = express.Router()
 
 router.get('/',function (req,res){
     res.render('index.html',{
-        hotkey:['aaa','bbb','ccc','ddd','eee']
+        hotkey:['aaa','bbb','ccc','ddd','eee'],
+        suggestions:['France','Allemagne','Chine','Italie']
     })
 })
 
